a7b1e149f679de28848972f8c2fd156bea773f2c
Paige Wielgos <paigewielgos@Paiges-MacBook-Pro.local>
2019-12-03 13:00:35 -0600

    first commit

---
+++ Prime/tier2/week-5/react-actors/src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+import Footer from '../Footer/Footer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
